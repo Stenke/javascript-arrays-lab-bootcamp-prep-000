5f77401796c66a42f5db7a4a15aa8714eee46405
Stephen Enke <stephenenke@gmail.com>
2020-08-20 21:12:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,5 +14,6 @@ function destructivelyRemoveFirstKitten() {
   return kittens.shift()
 }
 function appendKitten(name) {
- return appendKitten(kittens)
+ var moreKittens = 
+  appendKitten(kittens)
 }
